8c965a781591d4bbbf45545eeb0d9bdc4252ef20
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-10 19:02:49 -0300

    First commit - Project criated

---
+++ src/test/java/com/eduardocaio/projectmong/ProjectmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardocaio.projectmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
