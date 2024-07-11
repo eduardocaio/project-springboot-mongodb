9555c7c25bd74798d80793e374cfb156a9b4c1bc
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-11 17:14:08 -0300

    Referencing posts to User

--- src/main/java/com/eduardocaio/projectmong/config/Instantiation.java
+++ src/main/java/com/eduardocaio/projectmong/config/Instantiation.java
@@ -15,35 +15,36 @@ import com.eduardocaio.projectmong.domain.User;
 import com.eduardocaio.projectmong.dto.AuthorDTO;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
-	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	PostRepository postRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
-		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-	}
-
+    @Autowired
+    UserRepository userRepository;
+    
+    @Autowired
+    PostRepository postRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+        
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        
+        postRepository.saveAll(Arrays.asList(post1, post2));
+        
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria); 
+    }
 }
+
