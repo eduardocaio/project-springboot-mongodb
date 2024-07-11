18bd9f654f355f160912609f07c09db87127f912
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-11 14:44:34 -0300

    User findById

--- src/main/java/com/eduardocaio/projectmong/services/UserService.java
+++ src/main/java/com/eduardocaio/projectmong/services/UserService.java
@@ -1,12 +1,14 @@
 package com.eduardocaio.projectmong.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardocaio.projectmong.Repository.UserRepository;
 import com.eduardocaio.projectmong.domain.User;
+import com.eduardocaio.projectmong.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
