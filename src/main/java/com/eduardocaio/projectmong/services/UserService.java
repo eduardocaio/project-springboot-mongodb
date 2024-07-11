cdb12f2836c5cb06f3d16c5e594d659c5679c5f6
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-11 14:56:33 -0300

    Insertion User with POST

--- src/main/java/com/eduardocaio/projectmong/services/UserService.java
+++ src/main/java/com/eduardocaio/projectmong/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eduardocaio.projectmong.Repository.UserRepository;
 import com.eduardocaio.projectmong.domain.User;
+import com.eduardocaio.projectmong.dto.UserDTO;
 import com.eduardocaio.projectmong.services.exception.ObjectNotFoundException;
 
 @Service
@@ -23,6 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
