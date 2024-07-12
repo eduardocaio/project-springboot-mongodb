b34314c060564de17e8bcb2ceb0d319c464d7d27
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-12 10:55:25 -0300

    Simple consult with query methods

--- src/main/java/com/eduardocaio/projectmong/Repository/PostRepository.java
+++ src/main/java/com/eduardocaio/projectmong/Repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.eduardocaio.projectmong.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.eduardocaio.projectmong.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/eduardocaio/projectmong/services/PostService.java
+++ src/main/java/com/eduardocaio/projectmong/services/PostService.java
@@ -1,5 +1,6 @@
 package com.eduardocaio.projectmong.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 	
 	
 }
